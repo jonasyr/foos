cc64e483524c8e4e3015f6c683d8c09ac5a848bf
Jonas Weirauch <jonas.weirauch@gmx.de>
2025-11-03 17:19:07 +0100

    Refactor OK button handling to integrate menu toggle functionality; update key mapping and event processing for improved clarity and functionality.

--- plugins/control.py
+++ plugins/control.py
@@ -4,6 +4,7 @@ from .buttons import *
 class Plugin(Buttons):
     def __init__(self, bus):
         super().__init__(bus, long_press_delay=0.6)
+        self.menu_visible = False
 
     def generateKeyMap(self):
         key_map = {}
@@ -11,8 +12,7 @@ class Plugin(Buttons):
                   up(['black_plus'], ('increment_score', {'team': 'black'})),
                   up(['yellow_minus'], ('decrement_score', {'team': 'yellow'})),
                   up(['yellow_plus'], ('increment_score', {'team': 'yellow'})),
-                  up(['ok'], ('replay_request', {}), long=('upload_request', {})),
-                  down(['ok'], ('button_will_replay', {}), long=('button_will_upload', {})),
+                  # OK button handled in process_event (short=menu toggle, long=handled by replay_bridge)
                   up(['black_minus', 'black_plus'], ('reset_score', {}), long=None),
                   up(['yellow_minus', 'yellow_plus'], ('reset_score', {}), long=None),
                   down(['black_minus', 'black_plus'], None, long=('menu_show', {})),
@@ -22,8 +22,16 @@ class Plugin(Buttons):
 
     def process_event(self, ev):
         if ev.name == 'menu_visible':
+            self.menu_visible = True
             self.setEnabled(False)
         elif ev.name == 'menu_hidden':
+            self.menu_visible = False
             self.setEnabled(True)
+        elif ev.name == 'button_event' and ev.data.get('btn') == 'ok':
+            # OK button short press toggles menu
+            if self.menu_visible:
+                self.bus.notify('menu_hide', {})
+            else:
+                self.bus.notify('menu_show', {})
         else:
             super().process_event(ev)
